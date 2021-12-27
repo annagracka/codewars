8e70d49d3ea8f35156f594dedd7a9009dc6ded02
zigzagwanderer <an.noisens@gmail.com>
2021-12-27 23:33:35 +0100

    init zadanka

---
+++ convert_string_to_camel_case.js
@@ -0,0 +1,15 @@
+function toCamelCase(str){
+    if (str.length > 0) {
+        const wordsList = str.toLowerCase().split(/\W|\_/gi)
+        for (i in wordsList) {
+            wordsList[i]  = wordsList[i].charAt(0).toUpperCase() + wordsList[i].slice(1);
+            str = wordsList.join('').replace(wordsList[0].charAt(0), str.charAt(0));
+        };
+    } return str;
+}
+
+
+console.log(toCamelCase('')) //, '', "An empty string was provided but not returned")
+console.log(toCamelCase("the_stealth_warrior")) //, "theStealthWarrior", "toCamelCase('the_stealth_warrior') did not return correct value")
+console.log(toCamelCase("The-Stealth-Warrior")) //, "TheStealthWarrior", "toCamelCase('The-Stealth-Warrior') did not return correct value")
+console.log(toCamelCase("A-B-C")) //, "ABC", "toCamelCase('A-B-C') did not return correct value")
